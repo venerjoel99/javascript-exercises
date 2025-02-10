dd5120ae93e28c56c13f304d5e061e05edc3f82e
venerjoel99 <84veneracionj@gmail.com>
2025-02-09 20:45:36 -0500

    Exercise 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    const reversedArray = stringArray.reverse();
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
 const removeFromArray = function() {
+    if (arguments.length == 0) {
+        return [];
+    }
+    if (arguments.length == 1) {
+        return arguments[0];
+    }
+    let inputArray = arguments[0];
+    let itemsToRemove = Array.from(arguments);
+    itemsToRemove.shift();
+    let resultArray = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        const currentItem = inputArray[i];
+        const argItemIndex = itemsToRemove.indexOf(currentItem);
+        if (argItemIndex < 0) {
+            resultArray.push(currentItem);
+        }
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+removeFromArray([1,2,3], 3)
