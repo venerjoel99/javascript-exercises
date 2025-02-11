0c873da2073d8cb94d46b1e085d0f51e6c285ebf
venerjoel99 <84veneracionj@gmail.com>
2025-02-10 22:25:35 -0500

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const isValid = function(num) {
+    return typeof(num) === 'number' && num >= 0 && Math.floor(num) == num;
+}
 
+const sumAll = function(firstNum, secondNum) {
+    if (!isValid(firstNum) || !isValid(secondNum)) {
+        return "ERROR"
+    }
+    const start = Math.min(firstNum, secondNum);
+    const finish = Math.max(firstNum, secondNum);
+    let resultingSum = 0;
+    for (let currentNum = start; currentNum <= finish; currentNum++) {
+        resultingSum += currentNum;
+    }
+    return resultingSum;
 };
 
 // Do not edit below this line
