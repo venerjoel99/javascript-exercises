4e10b5def108e5132ab8b22cd8984e8a0c0f5f06
venerjoel99 <84veneracionj@gmail.com>
2025-02-23 01:17:39 -0500

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
 const findTheOldest = function() {
-
+    const inputArray = arguments[0];
+    return inputArray.reduce((first, second) => {
+        const currentYear = (new Date()).getFullYear();
+        const firstDeathYear = first.yearOfDeath === undefined ? currentYear : first.yearOfDeath;
+        const secondDeathYear = second.yearOfDeath === undefined ? currentYear : second.yearOfDeath;
+        const firstAge  = firstDeathYear - first.yearOfBirth;
+        const secondAge = secondDeathYear - second.yearOfBirth;
+        return firstAge > secondAge ? first : second;
+    }, inputArray[0].name);
 };
 
 // Do not edit below this line
