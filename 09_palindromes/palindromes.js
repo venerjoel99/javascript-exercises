82906dfeff798f23e09c1d9217ab23936bccb711
venerjoel99 <84veneracionj@gmail.com>
2025-02-23 00:39:35 -0500

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
 const palindromes = function () {
-
+    const filteredInput = arguments[0].toLowerCase().split("").filter(char => {
+        return ('a' <= char && char <= 'z') || ('0' <= char && char <= '9');
+    }).join("");
+    const reversedString = filteredInput.split("").reverse().join("");
+    return filteredInput === reversedString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
